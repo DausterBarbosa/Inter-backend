67d9853a33d102b6fc7c3583e2a3abb39c50d916
dausterbarbosa <dausterbarbosa12@gmail.com>
2021-12-17 22:59:45 -0300

    ormconfig setting

--- ormconfig.ts
+++ ormconfig.ts
@@ -10,7 +10,7 @@ module.exports = {
     "synchronize": true,
     "logging": false,
     "entities": [
-       "**/src/database/entity/**/*.js"
+       "build/src/database/entity/*.js"
     ],
     "migrations": [
        "src/database/migration/**/*.js"
